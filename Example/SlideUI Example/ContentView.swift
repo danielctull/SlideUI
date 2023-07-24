61eded725d7059c84a64be2fbde8ba74cc0b18a9
Daniel Tull <dt@danieltull.co.uk>
2023-07-24 19:02:36 +0100

    Add TokenView to display syntax tokens

--- Example/SlideUI Example/ContentView.swift
+++ Example/SlideUI Example/ContentView.swift
@@ -7,14 +7,84 @@ struct ContentView: View {
     var body: some View {
         Presentation {
             Slide(header: "Slide 1") {
+
+                TokenView(tokens: [
+                    .comment("// This is a comment"),
+                    .plain("\n"),
+                    .keyword("func"),
+                    .plain(" "),
+                    .identifierFunction("foo"),
+                    .plain("() {\n"),
+                    .plain("    "),
+                    .identifierFunctionSystem("print"),
+                    .plain("("),
+                    .string(#""Hello, world!""#),
+                    .plain(")\n"),
+                    .plain("}"),
+                ])
+            }
+            Slide(header: "Slide 2") {
                 Text("This is how to make a red square.")
                 #Code(Color.red.frame(width: 100, height: 100))
             }
-            Slide(header: "Slide 2") { Text("This is some content two") }
             Slide(header: "Slide 3") { Text("This is some content three") }
         }
+        .codeStyle(.mine)
         .slideStyle(CustomSlideStyle())
     }
+}
+
+extension CodeStyle {
+
+    static let mine = CodeStyle { token in
+        switch token.kind {
+        case .attribute: Color(red: 0.505801, green: 0.371396, blue: 0.012096, opacity: 1)
+        case .character: Color(red: 0.11, green: 0, blue: 0.81, opacity: 1)
+        case .comment: Color(red: 0, green: 0.456, blue: 0, opacity: 1)
+        case .commentDoc: Color(red: 0, green: 0.456, blue: 0, opacity: 1)
+        case .commentDocKeyword: Color(red: 0.008, green: 0.239, blue: 0.063, opacity: 1)
+        case .declarationOther: Color(red: 0.0588235, green: 0.407843, blue: 0.627451, opacity: 1)
+        case .declarationType: Color(red: 0.0431373, green: 0.309804, blue: 0.47451, opacity: 1)
+        case .identifierClass: Color(red: 0.109812, green: 0.272761, blue: 0.288691, opacity: 1)
+        case .identifierClassSystem: Color(red: 0.224543, green: 0, blue: 0.628029, opacity: 1)
+        case .identifierConstant: Color(red: 0.194184, green: 0.429349, blue: 0.454553, opacity: 1)
+        case .identifierConstantSystem: Color(red: 0.421903, green: 0.212783, blue: 0.663785, opacity: 1)
+        case .identifierFunction: Color(red: 0.194184, green: 0.429349, blue: 0.454553, opacity: 1)
+        case .identifierFunctionSystem: Color(red: 0.421903, green: 0.212783, blue: 0.663785, opacity: 1)
+        case .identifierMacro: Color(red: 0.391471, green: 0.220311, blue: 0.124457, opacity: 1)
+        case .identifierMacroSystem: Color(red: 0.391471, green: 0.220311, blue: 0.124457, opacity: 1)
+        case .identifierType: Color(red: 0.109812, green: 0.272761, blue: 0.288691, opacity: 1)
+        case .identifierTypeSystem: Color(red: 0.224543, green: 0, blue: 0.628029, opacity: 1)
+        case .identifierVariable: Color(red: 0.194184, green: 0.429349, blue: 0.454553, opacity: 1)
+        case .identifierVariableSystem: Color(red: 0.421903, green: 0.212783, blue: 0.663785, opacity: 1)
+        case .keyword: Color(red: 0.607592, green: 0.137526, blue: 0.576284, opacity: 1)
+        case .mark: Color(red: 0.14902, green: 0.458824, blue: 0.027451, opacity: 1)
+        case .markupCode: Color(red: 0.665, green: 0.052, blue: 0.569, opacity: 1)
+        case .number: Color(red: 0.11, green: 0, blue: 0.81, opacity: 1)
+        case .plain: Color(red: 0, green: 0, blue: 0, opacity: 0.85)
+        case .preprocessor: Color(red: 0.391471, green: 0.220311, blue: 0.124457, opacity: 1)
+        case .regex: Color(red: 0.77, green: 0.102, blue: 0.086, opacity: 1)
+        case .regexCapturename: Color(red: 0.194184, green: 0.429349, blue: 0.454553, opacity: 1)
+        case .regexCharname: Color(red: 0.421903, green: 0.212783, blue: 0.663785, opacity: 1)
+        case .regexNumber: Color(red: 0.11, green: 0, blue: 0.81, opacity: 1)
+        case .regexOther: Color(red: 0, green: 0, blue: 0, opacity: 0.85)
+        case .string: Color(red: 0.77, green: 0.102, blue: 0.086, opacity: 1)
+        case .url: Color(red: 0.055, green: 0.055, blue: 1, opacity: 1)
+        default: Color(red: 0, green: 0, blue: 0, opacity: 0.85)
+        }
+    } font: { token in
+        switch token.kind {
+        case .comment: .system(size: .font, weight: .light, design: .monospaced)
+        case .commentDoc: .system(size: .font, weight: .light, design: .monospaced).italic()
+        case .commentDocKeyword: .system(size: .font, weight: .light, design: .monospaced).italic()
+        case .mark: .system(size: .font, weight: .semibold, design: .monospaced)
+        default: .system(size: .font, weight: .regular, design: .monospaced)
+        }
+    }
+}
+
+extension CGFloat {
+    fileprivate static let font = Self(40)
 }
 
 struct CustomSlideStyle: SlideStyle {
@@ -38,8 +108,8 @@ struct CustomSlideStyle: SlideStyle {
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .padding(30)
-        .foregroundColor(.white)
-        .background(.black)
+        .foregroundColor(.black)
+        .background(.white)
     }
 }
 
--- Sources/SlideUI/Code.swift
+++ Sources/SlideUI/Code.swift
@@ -25,3 +25,151 @@ public struct CodeView: View {
         }
     }
 }
+
+public struct TokenView: View {
+    
+    @Environment(\.codeStyle) var style
+    let tokens: [Token]
+    
+    public init(tokens: [Token]) {
+        self.tokens = tokens
+    }
+    
+    public var body: some View {
+        tokens.reduce(Text("")) { accumulated, token in
+            let text: Text = Text(token.value)
+                .font(style.font(token))
+                .foregroundColor(style.color(token))
+            return accumulated + text
+        }
+    }
+}
+
+// MARK: - Token
+
+public struct Token {
+    public let value: String
+    public let kind: Kind
+}
+
+extension Token {
+
+    public struct Kind: Equatable, Hashable, Sendable {
+        private let name: String
+        private init(_ name: String) {
+            self.name = name
+        }
+    }
+}
+
+extension Token.Kind {
+    public static let attribute = Self("attribute")
+    public static let character = Self("character")
+    public static let comment = Self("comment")
+    public static let commentDoc = Self("comment.doc")
+    public static let commentDocKeyword = Self("comment.doc.keyword")
+    public static let declarationOther = Self("declaration.other")
+    public static let declarationType = Self("declaration.type")
+    public static let identifierClass = Self("identifier.class")
+    public static let identifierClassSystem = Self("identifier.class.system")
+    public static let identifierConstant = Self("identifier.constant")
+    public static let identifierConstantSystem = Self("identifier.constant.system")
+    public static let identifierFunction = Self("identifier.function")
+    public static let identifierFunctionSystem = Self("identifier.function.system")
+    public static let identifierMacro = Self("identifier.macro")
+    public static let identifierMacroSystem = Self("identifier.macro.system")
+    public static let identifierType = Self("identifier.type")
+    public static let identifierTypeSystem = Self("identifier.type.system")
+    public static let identifierVariable = Self("identifier.variable")
+    public static let identifierVariableSystem = Self("identifier.variable.system")
+    public static let keyword = Self("keyword")
+    public static let mark = Self("mark")
+    public static let markupCode = Self("markup.code")
+    public static let number = Self("number")
+    public static let plain = Self("plain")
+    public static let preprocessor = Self("preprocessor")
+    public static let regex = Self("regex")
+    public static let regexCapturename = Self("regex.capturename")
+    public static let regexCharname = Self("regex.charname")
+    public static let regexNumber = Self("regex.number")
+    public static let regexOther = Self("regex.other")
+    public static let string = Self("string")
+    public static let url = Self("url")
+}
+
+extension Token {
+    public static func attribute(_ value: String) -> Self { Self(value: value, kind: .attribute) }
+    public static func character(_ value: String) -> Self { Self(value: value, kind: .character) }
+    public static func comment(_ value: String) -> Self { Self(value: value, kind: .comment) }
+    public static func commentDoc(_ value: String) -> Self { Self(value: value, kind: .commentDoc) }
+    public static func commentDocKeyword(_ value: String) -> Self { Self(value: value, kind: .commentDocKeyword) }
+    public static func declarationOther(_ value: String) -> Self { Self(value: value, kind: .declarationOther) }
+    public static func declarationType(_ value: String) -> Self { Self(value: value, kind: .declarationType) }
+    public static func identifierClass(_ value: String) -> Self { Self(value: value, kind: .identifierClass) }
+    public static func identifierClassSystem(_ value: String) -> Self { Self(value: value, kind: .identifierClassSystem) }
+    public static func identifierConstant(_ value: String) -> Self { Self(value: value, kind: .identifierConstant) }
+    public static func identifierConstantSystem(_ value: String) -> Self { Self(value: value, kind: .identifierConstantSystem) }
+    public static func identifierFunction(_ value: String) -> Self { Self(value: value, kind: .identifierFunction) }
+    public static func identifierFunctionSystem(_ value: String) -> Self { Self(value: value, kind: .identifierFunctionSystem) }
+    public static func identifierMacro(_ value: String) -> Self { Self(value: value, kind: .identifierMacro) }
+    public static func identifierMacroSystem(_ value: String) -> Self { Self(value: value, kind: .identifierMacroSystem) }
+    public static func identifierType(_ value: String) -> Self { Self(value: value, kind: .identifierType) }
+    public static func identifierTypeSystem(_ value: String) -> Self { Self(value: value, kind: .identifierTypeSystem) }
+    public static func identifierVariable(_ value: String) -> Self { Self(value: value, kind: .identifierVariable) }
+    public static func identifierVariableSystem(_ value: String) -> Self { Self(value: value, kind: .identifierVariableSystem) }
+    public static func keyword(_ value: String) -> Self { Self(value: value, kind: .keyword) }
+    public static func mark(_ value: String) -> Self { Self(value: value, kind: .mark) }
+    public static func markupCode(_ value: String) -> Self { Self(value: value, kind: .markupCode) }
+    public static func number(_ value: String) -> Self { Self(value: value, kind: .number) }
+    public static func plain(_ value: String) -> Self { Self(value: value, kind: .plain) }
+    public static func preprocessor(_ value: String) -> Self { Self(value: value, kind: .preprocessor) }
+    public static func regex(_ value: String) -> Self { Self(value: value, kind: .regex) }
+    public static func regexCapturename(_ value: String) -> Self { Self(value: value, kind: .regexCapturename) }
+    public static func regexCharname(_ value: String) -> Self { Self(value: value, kind: .regexCharname) }
+    public static func regexNumber(_ value: String) -> Self { Self(value: value, kind: .regexNumber) }
+    public static func regexOther(_ value: String) -> Self { Self(value: value, kind: .regexOther) }
+    public static func string(_ value: String) -> Self { Self(value: value, kind: .string) }
+    public static func url(_ value: String) -> Self { Self(value: value, kind: .url) }
+}
+
+// MARK: - Code Style
+
+extension View {
+
+    public func codeStyle(_ style: CodeStyle) -> some View {
+        environment(\.codeStyle, style)
+    }
+}
+
+private struct CodeStyleKey: EnvironmentKey {
+    static var defaultValue = CodeStyle.plain
+}
+
+extension EnvironmentValues {
+
+    fileprivate var codeStyle: CodeStyle {
+        get { self[CodeStyleKey.self] }
+        set { self[CodeStyleKey.self] = newValue }
+    }
+}
+
+public struct CodeStyle {
+
+    let color: (Token) -> Color
+    let font: (Token) -> Font
+
+    public init(
+        color: @escaping (Token) -> Color,
+        font: @escaping (Token) -> Font
+    ) {
+        self.color = color
+        self.font = font
+    }
+}
+
+extension CodeStyle {
+
+    static let plain = Self(
+        color: { _ in .black },
+        font: { _ in .system(size: 18, weight: .regular, design: .monospaced) })
+}
