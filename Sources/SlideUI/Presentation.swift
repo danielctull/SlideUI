5fbbb36699c3216f22fecc53cee240cc0570f387
Daniel Tull <dt@danieltull.co.uk>
2023-09-15 16:37:49 +0100

    Add window for controlling the presentation

--- Sources/SlideUI/Presentation.swift
+++ Sources/SlideUI/Presentation.swift
@@ -18,28 +18,30 @@ extension Presentation {
     }
 }
 
+private let controlDeckID = UUID().uuidString
+
 private struct PresentationBody<Slides: View>: Scene {
 
+    @Environment(\.openWindow) private var openWindow
     @State private var deck = Deck()
     let slides: Slides
 
     public var body: some Scene {
+        
         WindowGroup {
             ZStack {
                 slides
             }
-            .overlay {
-                HStack(spacing: 0) {
-                    Color.clear
-                        .contentShape(Rectangle())
-                        .onTapGesture { deck.previous() }
-                    Color.clear
-                        .contentShape(Rectangle())
-                        .onTapGesture { deck.next() }
-                }
-            }
             .deck { deck = $0 }
             .environment(\.currentSlide, deck.current)
+            .onAppear { openWindow(id: controlDeckID) }
+        }
+
+        WindowGroup(id: controlDeckID) {
+            HStack {
+                Button("Previous") { deck.previous() }
+                Button("Next") { deck.next() }
+            }
         }
     }
 }
