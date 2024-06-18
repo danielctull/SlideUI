61e8859df0558ae0a14b68c5ce729d539cee596f
Daniel Tull <dt@danieltull.co.uk>
2024-06-18 10:46:00 +0100

    Merge Code macro files

--- Sources/SlideUIMacros/CodeMacro.swift
+++ Sources/SlideUIMacros/CodeMacro.swift
@@ -1,6 +1,32 @@
-
+import SwiftFormat
 import SwiftSyntax
 import SwiftSyntaxMacros
+
+struct Failure: Error {
+    let description: String
+}
+
+public struct LegacyCodeMacro: ExpressionMacro {
+
+    public static func expansion(
+        of node: some FreestandingMacroExpansionSyntax,
+        in context: some MacroExpansionContext
+    ) throws -> ExprSyntax {
+
+        guard let closure = node.trailingClosure else {
+            throw Failure(description: "Does not have a trailing closure.")
+        }
+
+        var output = ""
+        let formatter = SwiftFormatter(configuration: .init())
+        let file = SourceFileSyntax(statements: closure.statements)
+        try formatter.format(syntax: file, operatorTable: .init(), assumingFileURL: nil, to: &output)
+
+        return """
+            LegacyCode { \(literal: output) }
+            """
+    }
+}
 
 public struct CodeMacro: ExpressionMacro {
 
