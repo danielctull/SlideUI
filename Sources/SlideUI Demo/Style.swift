5bc05d30081ec100285d170d9e043b8b5bcba2a8
Daniel Tull <dt@danieltull.co.uk>
2024-06-19 07:30:39 +0100

    Make demo target

---
+++ Sources/SlideUI Demo/Style.swift
@@ -0,0 +1,35 @@
+import SlideUI
+import SwiftUI
+import XcodeCodeHighlighting
+
+extension SlideStyle where Self == CustomSlideStyle {
+    static var custom: CustomSlideStyle { CustomSlideStyle() }
+}
+
+struct CustomSlideStyle: SlideStyle {
+
+    func makeBody(configuration: Configuration) -> some View {
+        VStack {
+
+            configuration.header
+                .fontWidth(.expanded)
+                .textCase(.uppercase)
+                .font(.system(size: 100, weight: .ultraLight))
+                .foregroundColor(.yellow)
+                .frame(maxWidth: .infinity, alignment: .leading)
+
+            configuration.content
+                .font(.system(size: 60, weight: .light))
+                .frame(maxHeight: .infinity)
+
+            configuration.footer
+                .font(.system(size: 30, weight: .light))
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .padding(30)
+        .foregroundColor(.black)
+        .background(.white)
+        .codeStyle(.vertical)
+        .codeHighlighting(.xcodeClassicLight)
+    }
+}
