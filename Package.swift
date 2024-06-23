e2d1a9bbd9587a50aa6fca3f552a186de243e851
Daniel Tull <dt@danieltull.co.uk>
2024-06-23 10:39:13 +0100

    Update to latest OS releases

--- Package.swift
+++ Package.swift
@@ -6,10 +6,10 @@ import PackageDescription
 let package = Package(
     name: "SlideUI",
     platforms: [
-        .iOS(.v16),
-        .macOS(.v13),
-        .tvOS(.v16),
-        .watchOS(.v9),
+        .iOS(.v17),
+        .macOS(.v14),
+        .tvOS(.v17),
+        .watchOS(.v10),
     ],
     products: [
         .library(
